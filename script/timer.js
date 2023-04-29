5b7a1b78271f9c1cc3c9b9d9b5ef005b855e6f21
sustaaah <64201334+sustaaah@users.noreply.github.com>
2023-04-29 09:45:58 +0000

    saving

---
+++ script/timer.js
@@ -0,0 +1,21 @@
+function p10() {
+	iteration = 0;
+	while (iteration = 5) {
+      iteration++;
+   }
+   
+}
+
+function psp() {
+   	iteration = 0;
+	while (iteration = 5) {
+		//attention
+
+		// shoot
+		iteration++;
+   }
+}
+
+function ps() {
+
+}
